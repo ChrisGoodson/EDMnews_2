1f4d31699b1ea2e2e3a2f5ee0fbe31ec83382ed0
Chris <f.chrisgoodson@icloud.com>
2023-07-05 17:21:11 -0500

    Initial Commit

---
+++ EDMnews/EDMnewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EDMnewsApp.swift
+//  EDMnews
+//
+//  Created by Chris on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct EDMnewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
